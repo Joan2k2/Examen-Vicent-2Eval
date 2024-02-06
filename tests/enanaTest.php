5fae168752f107e0c083cc522e653ac90bb39e7e
Joan2k2 <joanruizmartinez@gmail.com>
2024-02-06 12:26:42 +0100

    herida leve muerta

--- tests/enanaTest.php
+++ tests/enanaTest.php
@@ -25,7 +25,10 @@ class EnanaTest extends TestCase {
     public function testHeridaLeveMuere() {
         #Se probará el efecto de una herida leve a una Enana con puntos de vida insuficientes para sobrevivir al ataque
         #Se tendrá que probar que la vida es menor que 0 y además que su situación es muerta
-
+        $enana1 = new Enana("Eanana1",9);
+        $enana1->heridaLeve();
+        $this->assertEquals(-1, $enana1->getPuntosVida());
+        $this->assertEquals("muerta", $enana1->getSituacion());
     }
 
     public function testHeridaGrave() {
